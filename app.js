65d0ba8eb9d326c2ddc28261e91517e73be2dc8d
vovanolo <vpankiv@softserveinc.com>
2023-11-02 17:32:36 +0200

    lesson1

---
+++ app.js
@@ -0,0 +1,79 @@
+// console.log("hello world!");  
+
+// console.log("hello world!"); // here is an output of hello world
+
+// // here is my comment
+
+
+
+// console.log(some);
+
+// variables - place to store the data
+
+// var  (const let)
+
+// let message = "hello world!"; // can be changed
+// const VARIABLE = "hello";
+
+
+// message = "hello guys";
+// message = 25;
+//  " " - string - represt the text
+// dynamic typed language
+// message = 25;
+// 1 word - lovercase
+// 2 or more words - camelCase or PascalCase
+
+// myNameIsCorrect - camelCase
+// myNameisCorrect - PascalCase
+
+// console.log(message);  
+
+
+// Data types
+
+// 
+
+// console.log("hello" + "world!");
+
+// let name = 5;
+
+// let name = true;
+
+
+// console.log(`My name is ${name}`);
+
+// let x = "ssfsf";
+
+// console.log(typeof x);
+
+
+// interaction with user
+
+
+// alert()
+
+// let name = "John"
+
+// alert(`my name is ${name}`)
+
+// prompt()
+
+// let name = prompt("Please enter your name");
+
+// console.log(name);
+
+// confirm()
+
+// let isOk = confirm("Are you okay?");
+
+// console.log(isOk);
+
+// task2
+
+// ...
+
+// task3
+
+// ....
+
