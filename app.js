fbf8d8d80d823ee23728259201b8d4ab73355856
vovanolo <vpankiv@softserveinc.com>
2023-11-09 17:46:11 +0200

    lesson2

--- app.js
+++ app.js
@@ -77,3 +77,112 @@
 
 // ....
 
+// Math
+
+// 2 ** 4
+
+// 4 ** (1/2)
+// Math.sqrt(4);
+// Math.pow(2, 4);
+
+
+// console.log(10 + "5"); // "105"
+
+// 1 - true
+// 0 - false
+// "" - false
+// "sfs" - true
+
+
+// let age = prompt("How old are you");
+
+// let result = (age > 21) ? "You are 21+" : "You are 21-";
+
+// alert(result);
+
+
+const people = ["John", "Peter", "Bob", "Doe"];
+
+// console.log(people);
+// console.log(people[1]);
+// people[1] = "Mykola";
+// console.log(people);
+
+// console.log(people[5]);
+
+// people[4] = "Ivanna";
+
+// people[8] = "Virginia"
+
+// console.log(people);
+
+// console.log(people.length);
+// people.push("Virginia");
+
+// console.log(people);
+
+// people.pop();
+
+// console.log(people);
+
+// people.unshift("Olha");
+
+// console.log(people);
+// let age = +prompt("How old are you");
+
+// if (age > 21) {
+//     console.log("age is 21 +");
+// }
+
+// if (age > 21) {
+//     console.log("age is 21 +");
+// } else {
+//     console.log("age is 21-");
+// }
+
+// if (age > 21 && age < 30){
+//     console.log("age is 21-30");
+// }else if (age > 30 && age < 40){
+//     console.log("age is 30-40");
+// }else if (age > 40 && age < 50){
+//     console.log("age is 40-50");
+// }else{
+//     console.log(" age is 21- or 50+");
+// }
+
+
+// switch case
+
+
+// let age = +prompt("How old are you");
+
+// switch(age){
+//     case 21:
+//         console.log("age is 21");
+//         break;
+//     case 22:
+//         console.log("age is 22");
+//         break;
+//     default:
+//         console.log("age is not 21 or 22");
+        // break;
+// }
+
+
+// switch(true){
+//     case age > 21 && age < 30:
+//         console.log("age is 21-30");
+//         break;
+//     case age > 30 && age < 40:
+//         console.log("age is 30-40");
+//     default:
+//         console.log("age is not 21-40");
+//         break;
+// }
+
+
+// let age = +prompt("How old are you");
+
+// if (age !== 21){
+
+// }
